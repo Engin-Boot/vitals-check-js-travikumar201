e217eff93bba22271e382784647689eee5adf0dd
Ravikumar Tiwari <ravitiwari201@gmail.com>
2020-08-28 12:22:38 +0530

    Reduced cyclomatic complexity

--- checker.js
+++ checker.js
@@ -1,14 +1,27 @@
 const expect = require('chai').expect;
 
-function vitalsAreOk(bpm, spo2, respRate) {
-    if(bpm < 70 || bpm > 150) {
-        return false;
-    } else if(spo2 < 90) {
-        return false;
-    } else if(respRate < 30 || respRate > 95) {
+function checkBpm(bpm){
+    if(bpm < 70 || bpm > 150){
         return false;
     }
     return true;
+}
+
+function checkSpo2(spo2){
+    if(spo2 < 90){
+        return false;
+    }
+    return true;
+}
+
+function checkRespRate(respRate){
+    if(respRate < 30 || respRate > 95) {
+        return false;
+    }
+    return true;
+}
+function vitalsAreOk(bpm, spo2, respRate) {
+    return (checkBpm(bpm) && checkSpo2(spo2) && checkRespRate(respRate)); 
 }
 
 expect(vitalsAreOk(100, 95, 70)).to.be.true;
