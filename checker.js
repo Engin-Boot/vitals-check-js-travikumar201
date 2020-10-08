389302a6e16e41bdfef326abef2a127411f71a55
Ravikumar Tiwari <ravitiwari201@gmail.com>
2020-10-08 12:11:55 +0530

    SOLID compatible and makes use of JS function return feature

--- checker.js
+++ checker.js
@@ -1,55 +1,28 @@
 const expect = require('chai').expect;
+const checkRange = require('./check-range');
 
-function vitalsAreOk(bpm, spo2, respRate){
-    function checkBpm(bpm){
-        if(bpm < 70){
-            console.log(`Bpm is ` + bpm + `, which is less than 70`);
-            return false;
-        }
-        else if(bpm > 150){
-            console.log(`Bpm is ` + bpm + `, which is more than 150`);
-            return false;
-        }
-        return true;
-    }
-    
-    function checkSpo2(spo2){
-        if(spo2 < 90){
-            console.log(`Spo2 is ` + spo2 + `, which is less than 90`);
-            return false;
-        }
-        return true;
-    }
-    
-    function checkRespRate(respRate){
-        if(respRate < 30) {
-            console.log(`Respiration rate is ` + respRate + `, which is less than 30`);
-            return false;
-        }
-        else if(respRate > 95){
-            console.log(`Respiration rate is ` + respRate + `, which is more than 95`);
-            return false;
-        }
-        return true;
-    }
-    return (checkBpm(bpm) && checkSpo2(spo2) && checkRespRate(respRate));
+function checkBpm(){
+    let bpmIsInRange = checkRange.makeChecker(70, 150);
+    expect(bpmIsInRange("BPM", 100)).to.be.true;
+    expect(bpmIsInRange("BPM", 50)).to.be.false;
+    expect(bpmIsInRange("BPM", 170)).to.be.false;
 }
 
-// var doctor = new ReportGenerator();
-// var nurse = new ReportGenerator();
+function checkSpo2(){
+    let spo2IsInRange = checkRange.makeChecker(0, 90);
+    expect(spo2IsInRange("SPO2", 70)).to.be.true;
+    expect(spo2IsInRange("SPO2", -1)).to.be.false;
+    expect(spo2IsInRange("SPO2", 100)).to.be.false;
+}
 
+function checkRespRate(){
+    let respRateIsInRange = checkRange.makeChecker(30, 95);
+    expect(respRateIsInRange("Respiration Rate", 45)).to.be.true;
+    expect(respRateIsInRange("Respiration Rate", 23)).to.be.false;
+    expect(respRateIsInRange("Respiration Rate", 98)).to.be.false;
+}
 
-expect(vitalsAreOk(100,95,70)).to.be.true;
-expect(vitalsAreOk(50, 95, 70)).to.be.false;
-expect(vitalsAreOk(170, 95, 70)).to.be.false;
-expect(vitalsAreOk(80, 80, 70)).to.be.false;
-expect(vitalsAreOk(80, 95, 25)).to.be.false;
-expect(vitalsAreOk(80, 95, 100)).to.be.false;
-// checks for corner cases
-expect(vitalsAreOk(70, 95, 70)).to.be.true;
-expect(vitalsAreOk(150, 95, 70)).to.be.true;
-expect(vitalsAreOk(83,90,70)).to.be.true;
-expect(vitalsAreOk(92, 100, 30)).to.be.true;
-expect(vitalsAreOk(92, 100, 95)).to.be.true;
-
+checkBpm();
+checkSpo2();
+checkRespRate();
 console.log('checker is done');
